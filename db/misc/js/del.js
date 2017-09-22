9a8cc395958ec75c5aecb2fe91de35297ddd3b7c
joniibra <joniibra@users.noreply.github.com>
2017-09-22 07:46:01 +0700

    Add files via upload

---
+++ db/misc/js/del.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name JoniIbra Custom Userscript
+// @description This is JavaScript
+// @author Joni Ibra
+// @version 1.00
+// @encoding utf-8
+// @license GNU GPL v3
+// @icon https://cdn.rawgit.com/joniibra/pen/joniibra.png
+// @homepage https://joniibra.github.io/pen
+// @supportURL https://github.com/joniibra/pen/issues
+// @updateURL https://cdn.rawgit.com/joniibra/pen/db/empty.js
+// @downloadURL https://cdn.rawgit.com/joniibra/pen/db/empty.js
+// @include http://*/*
+// @include https://*/*
+// @grant unsafeWindow
+// @grant GM_addStyle
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_xmlhttpRequest
+// @grant GM_registerMenuCommand
+// @grant GM_deleteValue
+// @grant GM_listValues
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_log
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant GM_info
+// @grant GM_getMetadata
